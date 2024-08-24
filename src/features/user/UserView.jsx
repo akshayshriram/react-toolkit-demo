54e130572dde4963fa745cd5a2bf0651186b3189
Akshay <akshayshriram999@gmail.com>
2024-08-24 16:17:37 +0530

    Fetch User Data...

--- src/features/user/UserView.jsx
+++ src/features/user/UserView.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchUsers } from "./userSlice";
 
 const UserView = () => {
+  const user = useSelector((state) => state.user);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchUsers());
+  }, []);
+
   return (
     <div>
-        <h2>List Of Users : </h2>
+      <h2>List Of Users : </h2>
+      {user.loading && <p>Loading...</p>}
+      {!user.loading && user.error ? <p>Error: {user.error}</p> : null}
+      {!user.loading && user.users.length ? (
+        <ul>
+          {user.users.map((user) => (
+            <li key={user.id}>{user.name}</li>
+          ))}
+        </ul>
+      ) : null}
     </div>
-  )
-}
+  );
+};
 
-export default UserView
+export default UserView;
